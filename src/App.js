536349e9c9115a4f30899a6383a9c2f9cca260ae
Nicky <nickyliu91@gmail.com>
2020-06-21 23:41:51 -0400

    started basics of frontend

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LogIn from './LogIn.js';
+
+function App() {
+  return (
+    <div className="App">
+      <LogIn />
+    </div>
+  );
+}
+
+export default App;
