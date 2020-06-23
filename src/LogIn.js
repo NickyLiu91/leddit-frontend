d640a0851e84ff3568fd546c00a2fc8aafc15ab5
Nicky <nickyliu91@gmail.com>
2020-06-22 22:51:34 -0400

    added login form

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,10 +1,23 @@
 import React from "react";
 
 class LogIn extends React.Component {
+
+  logIN = () => {
+    fetch(`http://localhost:3000/api/v1/accounts`)
+    .then(res => res.json())
+    .then(json => {console.log(json)})
+  }
+
+
   render() {
     return(
       <div>
-        <p>hi</p>
+        <form>
+          <p>Account</p>
+          <input type="text"/>
+          <p>Password</p>
+          <input type="text"/>
+        </form>
       </div>
     )
   }
