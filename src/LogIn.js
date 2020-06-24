f867bf383df08a668b7241b255cea8a2e31ddaaf
Nicky <nickyliu91@gmail.com>
2020-06-23 22:34:28 -0400

    added state for name and password

--- src/LogIn.js
+++ src/LogIn.js
@@ -2,6 +2,11 @@ import React from "react";
 
 class LogIn extends React.Component {
 
+  state = {
+    name: '',
+    password: ''
+  }
+
   logIN = () => {
     fetch(`http://localhost:3000/api/v1/accounts`)
     .then(res => res.json())
@@ -14,14 +19,26 @@ class LogIn extends React.Component {
     .then(json => {console.log(json)})
   }
 
+  handleName = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handlePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
   render() {
     return(
       <div>
         <form>
-          <p>Account</p>
-          <input type="text"/>
-          <p>Password</p>
-          <input type="text"/>
+          Account: <input type="text" value={this.state.name} onChange={event => this.handleName(event)}/>
+          <br/>
+          <br/>
+          Password: <input type="password" value={this.state.password} onChange={event => this.handlePassword(event)}/>
         </form>
         <br />
         <button onClick={event => this.fetchy(event)}>CLICK ME</button>
