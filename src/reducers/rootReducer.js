565231c26b633273f6363f5caed062d3c4d073c1
Nicky <nickyliu91@gmail.com>
2020-07-28 23:44:33 -0400

    got root reducer instead

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,20 @@
+import {combineReducers} from 'redux'
+
+export default combineReducers({
+  accountChanger,
+  cardsChanger,
+  currentPlayerCardsChanger,
+  noDupesCurrentPlayerCardsChanger,
+  deckChanger,
+  deckCardsChanger,
+  charactersChanger,
+  enemyChanger,
+  enemyDeckChanger,
+  cardChanger,
+  decksListChanger,
+  goldChanger,
+  defeatedChanger,
+  dialogueChanger,
+  locationChanger,
+  completedChanger
+})
