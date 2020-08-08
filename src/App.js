9c8c49498c9f680071dcc1800d9e14366df9d4c0
Nicky <nickyliu91@gmail.com>
2020-08-07 23:04:19 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Switch, Redirect, withRouter} from 'react-router-dom'
+import Nav from './components/nav.js';
 import LogIn from './components/logIn.js';
 import Profile from './components/profile.js';
 import NotFound from './components/notFound.js';
@@ -10,6 +11,8 @@ import NotFound from './components/notFound.js';
     console.log(props)
     return(
       <div>
+        <Nav />
+        <br/>
         <Switch>
           <Route exact path="/" render={() => <Redirect to="/profile" />} />
 
---
+++ src/components/nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Nav extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <div>NAV1</div>
+        <div>NAV2</div>
+        <div>NAV3</div>
+      </div>
+    )
+  }
+}
+
+export default Nav
