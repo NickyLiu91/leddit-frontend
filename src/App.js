79af2e500e7c3e3059331651f0714611792377c4
Nicky <nickyliu91@gmail.com>
2020-10-19 22:56:33 -0400

    finished most of adding posts to default page

--- src/App.js
+++ src/App.js
@@ -3,34 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux'
 import {compose} from 'redux';
+import store from './index.js';
 import { Route, Switch, Redirect, withRouter} from 'react-router-dom'
 import Nav from './components/nav.js';
 import LogIn from './components/logIn.js';
 import Profile from './components/profile.js';
 import NotFound from './components/notFound.js';
 
-  const App = props => {
-    console.log(props)
-    return(
-      <div>
-        <Nav />
-        <br/>
-        <Switch>
-          <Route exact path="/" render={() => <Redirect to="/profile" />} />
+  class App extends React.Component {
 
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/login" component={LogIn} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    )
+    componentDidMount() {
+      fetch(`http://localhost:3000/api/v1/posts`)
+      .then(res => res.json())
+      .then(json => {this.props.changePosts(json)})
+    }
+
+    render(){
+      return (
+        <div>
+          <Nav />
+          <br/>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/profile" />} />
+
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/login" component={LogIn} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      )
+    }
   }
 
-const mapStateToProps = state => {
-  return {
-    posts: state.postsChanger.posts,
-  }
-}
+// const mapStateToProps = state => {
+//   return {
+//     posts: state.postsChanger.posts,
+//   }
+// }
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -41,6 +50,6 @@ const mapDispatchToProps = dispatch => {
 export default compose(
   withRouter,
   connect(
-  mapStateToProps,
+  // mapStateToProps,
   mapDispatchToProps)
 )(App);
