65a9edd050b61d85117b506257517d99d27839a0
Nicky <nickyliu91@gmail.com>
2020-10-19 23:23:48 -0400

    undid posts in app.js and moved to posts.js file

--- src/App.js
+++ src/App.js
@@ -1,55 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { connect } from 'react-redux'
-import {compose} from 'redux';
-import store from './index.js';
 import { Route, Switch, Redirect, withRouter} from 'react-router-dom'
 import Nav from './components/nav.js';
 import LogIn from './components/logIn.js';
 import Profile from './components/profile.js';
 import NotFound from './components/notFound.js';
 
-  class App extends React.Component {
+  const App = props => {
+    console.log(props)
+    return(
+      <div>
+        <Nav />
+        <br/>
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/profile" />} />
 
-    componentDidMount() {
-      fetch(`http://localhost:3000/api/v1/posts`)
-      .then(res => res.json())
-      .then(json => {this.props.changePosts(json)})
-    }
-
-    render(){
-      return (
-        <div>
-          <Nav />
-          <br/>
-          <Switch>
-            <Route exact path="/" render={() => <Redirect to="/profile" />} />
-
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/login" component={LogIn} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      )
-    }
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/login" component={LogIn} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    )
   }
 
-// const mapStateToProps = state => {
-//   return {
-//     posts: state.postsChanger.posts,
-//   }
-// }
-
-const mapDispatchToProps = dispatch => {
-  return {
-    changePosts: (event) => dispatch({type: 'CHANGE_POSTS', newPosts: event}),
-  }
-}
-
-export default compose(
-  withRouter,
-  connect(
-  // mapStateToProps,
-  mapDispatchToProps)
-)(App);
+export default withRouter(App);
