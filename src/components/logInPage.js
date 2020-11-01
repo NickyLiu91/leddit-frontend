aa8de67469f3a542196d02025a1be95efa33f815
Nicky <nickyliu91@gmail.com>
2020-11-01 05:28:38 -0500

    fixed up login page

--- src/components/logInPage.js
+++ src/components/logInPage.js
@@ -12,16 +12,7 @@ class LogInPage extends React.Component {
     if (Object.keys(this.props.account).length == 0) {
       return (
         <div>
-          <form onSubmit={this.handleLoginSubmit}>
-            Account: <input id="name" type="text" value={this.state.name} onChange={event => this.handleStuff(event)}/>
-            <br/>
-            <br/>
-            Password: <input id="password" type="password" value={this.state.password} onChange={event => this.handleStuff(event)}/>
-            <br/>
-            <br/>
-            <button type="submit">Login</button>
-          </form>
-          <button onClick={event => {console.log(this.props.account)}}>CLICK</button>
+          <LogIn />
         </div>
       )
     } else {
