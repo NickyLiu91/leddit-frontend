711a70198ea0c14dfd89b88018121fb78d0c7d30
Nicky <nickyliu91@gmail.com>
2020-11-01 05:21:41 -0500

    changed account to functional component

--- src/components/account.js
+++ src/components/account.js
@@ -1,34 +1,46 @@
+// import React from "react";
+// import { connect } from 'react-redux'
+// import {compose} from 'redux';
+// import { Route, Link, withRouter } from 'react-router-dom'
+//
+// class Account extends React.Component {
+//
+//   render() {
+//     if (Object.keys(this.props.account).length != 0) {
+//       return (
+//         <div>
+//           Name: {this.props.account.account.name}
+//         </div>
+//       )
+//     } else {
+//       return (
+//         <div>
+//           No Account Found
+//         </div>
+//       )
+//     }
+//   }
+// }
+//
+// const mapStateToProps = state => {
+//   return {
+//     account: state.accountChanger.account,
+//   }
+// }
+//
+// export default compose(
+//   withRouter,
+//   connect(mapStateToProps)
+// )(Account);
+
 import React from "react";
-import { connect } from 'react-redux'
-import {compose} from 'redux';
-import { Route, Link, withRouter } from 'react-router-dom'
 
-class Account extends React.Component {
-
-  render() {
-    if (Object.keys(this.props.account).length != 0) {
-      return (
-        <div>
-          Name: {this.props.account.account.name}
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          No Account Found
-        </div>
-      )
-    }
-  }
+const Account = (props) => {
+  return(
+    <div>
+      Name: {props.account.account.name}
+    </div>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    account: state.accountChanger.account,
-  }
-}
-
-export default compose(
-  withRouter,
-  connect(mapStateToProps)
-)(Account);
+export default Account;
