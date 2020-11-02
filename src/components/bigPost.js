5ee82829aa4793ab5ceae16137112764cefaf330
Nicky <nickyliu91@gmail.com>
2020-11-02 04:31:47 -0500

    set up most of comments

--- src/components/bigPost.js
+++ src/components/bigPost.js
@@ -2,21 +2,26 @@ import React from "react";
 import { connect } from 'react-redux'
 import {compose} from 'redux';
 import { Route, Link, withRouter } from 'react-router-dom'
+import Comment from './comment.js';
 
 class BigPost extends React.Component {
+
+  displayComments = () => {
+    let list = this.props.selectedPost.comments
+
+    return list.map(
+      comment => <Comment comment={comment} />
+    )
+  }
 
   render() {
     if (Object.keys(this.props.selectedPost).length != 0) {
       console.log(this.props.selectedPost)
       return (
         <div>
-        {this.props.selectedPost.content}
-        </div>
-      )
-    } else {
-      return(
-        <div>
-        TEST
+          <h1>{this.props.selectedPost.title}</h1>
+          <p>{this.props.selectedPost.content}</p>
+          {this.displayComments()}
         </div>
       )
     }
---
+++ src/components/comment.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Comment = (props) => {
+  return(
+    <div className="post">
+      <ul>
+        <li>
+          <h1>Comment</h1>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Comment;
