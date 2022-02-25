eebe757f837f84cd987fad9c0a831d955495bb9b
Nicky <nickyliu91@gmail.com>
2022-02-25 07:17:45 -0500

    added like and dislike counter for posts

--- src/App.js
+++ src/App.js
@@ -51,9 +51,13 @@ class App extends React.Component {
       })
     }
 
-    // fetch(`http://localhost:3000/api/v1/commentvotes`)
-    // .then(res => res.json())
-    // .then(json => {console.log(json)})
+    fetch(`http://localhost:3000/api/v1/commentvotes`)
+    .then(res => res.json())
+    .then(json => {console.log(json)})
+
+    fetch(`http://localhost:3000/api/v1/postvotes`)
+    .then(res => res.json())
+    .then(json => {console.log(json)})
   }
 
   fetchCurrentUser = () => {
--- src/components/post.js
+++ src/components/post.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Route, Link, withRouter } from 'react-router-dom'
 
 const Post = (props) => {
-
     return(
       <div className="post">
         <ul>
           <li>
             <h1 className="postTitle" onClick={() => {props.history.push(`/bigpost/${props.post.id}`)}}>{props.post.title}</h1>
             <p onClick={() => {props.history.push(`/account/${props.post.account.id}`)}}> Submitted on {props.post.created_at.slice(0, -14)} by {props.post.account.name}</p>
+            <div>
+              <p>Like: {props.post.postvotes.filter(vote => vote.like).length}</p>
+              <p>Dislike: {props.post.postvotes.filter(vote => !vote.like).length}</p>
+            </div>
           </li>
         </ul>
       </div>
